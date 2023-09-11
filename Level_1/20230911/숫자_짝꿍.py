d47e8addf75859cfe3970ef81cc36ccb5eda0db6
unknown <xodud602@gmail.com>
2023-09-11 21:55:45 +0900

    Level_1_practice_18

---
+++ Level_1/20230911/숫자_짝꿍.py
@@ -0,0 +1,9 @@
+def solution(X, Y):
+    answer = ''
+    for i in range(10) :
+        answer = (str(i) * min(X.count(str(i)), Y.count(str(i)))) + answer
+        
+    if len(answer) == 0 : return "-1"
+    return '0' if answer[0] == '0' else answer
+
+# functools 에서 Counter를 가져와서 해결할 수도 있음
