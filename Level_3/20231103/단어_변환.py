1256465fba2ea442349653853a16e0b49ac0bede
Egoty62 <myosotis62@naver.com>
2023-11-05 20:38:20 +0900

    Level_3_practice_3

--- Level_3/20231103/단어_변환.py
+++ Level_3/20231103/단어_변환.py
@@ -7,15 +7,14 @@ def solution(begin, target, words):
         visited = {i : [] for i in words}
         visited[begin] = []
         stack.append(begin)
-        while len(stack) :
+        while stack :
             last = stack[-1]
             if last == target :
                 answer.append(len(stack) - 1)
             else :
                 for k in [i for i in words if i not in stack] :
-                    print(k, stack)
-                    if k in visited[stack[-1]] :
-                        stack.pop()
+                    if k in visited[last] :
+                        continue
                     else :
                         diff = 0
                         for j in range(n) :
@@ -26,8 +25,7 @@ def solution(begin, target, words):
                             stack.append(k)
             if last == stack[-1] :
                 stack.pop()
-
-            #print(stack)
+            
     return min(answer) if answer else 0
 
-print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+# https://school.programmers.co.kr/learn/courses/30/lessons/43163
